68066dbdd9ed1b0d77e2ec7e747ddf43af26db3d
Lindsay Clark <lvclark@illinois.edu>
2021-12-06 21:37:40 -0600

    Run Rcpp::compileAttributes

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -5,6 +5,11 @@
 
 using namespace Rcpp;
 
+#ifdef RCPP_USE_GLOBAL_ROSTREAM
+Rcpp::Rostream<true>&  Rcpp::Rcout = Rcpp::Rcpp_cout_get();
+Rcpp::Rostream<false>& Rcpp::Rcerr = Rcpp::Rcpp_cerr_get();
+#endif
+
 // G
 int G(int q, int n);
 RcppExport SEXP _polysat_G(SEXP qSEXP, SEXP nSEXP) {
